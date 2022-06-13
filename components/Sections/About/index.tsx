9185c85c4515dbb1834e7e95a17f17ba71c604fc
Sureshkumar Karuppusamy <sureshkumarkdsk@gmail.com>
2022-06-13 13:57:11 +0530

    Moved components into about section

--- components/Sections/About/index.tsx
+++ components/Sections/About/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
-
-const About = () => {
-	return <div>About</div>;
+import Image from "next/image";
+import AboutContent from "./AboutContent";
+const AboutSection = () => {
+	return (
+		<div className="container flex flex-col space-y-5 py-5">
+			<h2 className="text-2xl mb-4 font-semibold">About Me</h2>
+			<Image
+				src={"/static/images/suresh.jpeg"}
+				alt={"Profile Photo"}
+				width={768}
+				height={320}
+				quality={100}
+				objectFit={"cover"}
+				objectPosition={"top"}
+				layout={"intrinsic"}
+			/>
+			{/* aboutcontent */}
+			<AboutContent />
+			{/* Experiences */}
+		</div>
+	);
 };
 
-export default About;
+export default AboutSection;
