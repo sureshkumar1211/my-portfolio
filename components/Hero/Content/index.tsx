2722eae48a1d17b7c55f4b145a604b4a5cf3aaa8
Sureshkumar Karuppusamy <sureshkumarkdsk@gmail.com>
2024-05-09 21:41:13 +0530

    Updated cloudinary images and some contents.

--- components/Hero/Content/index.tsx
+++ components/Hero/Content/index.tsx
@@ -2,25 +2,31 @@ import Link from "next/link";
 import React from "react";
 
 const contentObject = {
-	title: "Hey 👋!, I'm <span class='text-secondary-color'>Suresh</span>, Software Engineer",
-	description:
-		"I'm a self-taught developer who lives in India 🇮🇳. I've worked with different technologies, both backend and frontend. I'm currently working at Konnectify as a Software Engineer. You can read more about me on my LinkedIn page.",
+  title:
+    "Hey 👋!, I'm <span class='text-secondary-color'>Suresh</span>, Senior Software Engineer",
+  description:
+    "I'm an India-based self-taught developer skilled in various technologies spanning backend and frontend domains. Presently, I'm employed as a Senior Software Engineer at ZEE5. For further insights into my professional journey, feel free to explore my LinkedIn profile.",
 };
 
 const Content = () => {
-	return (
-		<div className="flex mt-5 md:m-0 text-center md:text-left md:w-[50%] flex-col space-y-5">
-			<h2 className="text-xl md:text-3xl" dangerouslySetInnerHTML={{ __html: contentObject.title }} />
-			<p className="text-sm font-light leading-relaxed text-gray-600">{contentObject.description}</p>
-			<div>
-				<Link href="/static/Suresh/Resume.pdf" target={"_blank"}>
-					<a className="mt-4 rounded-md hover:bg-primary-color text-sm px-4 py-2 bg-primary-color-700 text-white">
-						Download Resume
-					</a>
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex mt-5 md:m-0 text-center md:text-left md:w-[50%] flex-col space-y-5">
+      <h2
+        className="text-xl md:text-3xl"
+        dangerouslySetInnerHTML={{ __html: contentObject.title }}
+      />
+      <p className="text-sm font-light leading-relaxed text-gray-600">
+        {contentObject.description}
+      </p>
+      <div>
+        <Link href="/static/Suresh/Resume.pdf" target={"_blank"}>
+          <a className="mt-4 rounded-md hover:bg-primary-color text-sm px-4 py-2 bg-primary-color-700 text-white">
+            Download Resume
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
--- constants/pages/works.ts
+++ constants/pages/works.ts
@@ -1,38 +1,38 @@
 export const data = [
-	{
-		title: "Youtube",
-		link: "https://sureshkumar1211.github.io/youtube-search-api",
-		type: "personal",
-		img: "/static/images/youtube.png",
-		languages: ["javascript", "react", "redux"],
-		description:
-			"This app will helps to fetch related videos on YouTube for the given search query. I've used GCP apikey for authentication to fetch YouTube videos.",
-	},
-	{
-		title: "Todo List",
-		link: "https://sureshkumar1211.github.io/react-redux-todo",
-		type: "personal",
-		img: "/static/images/react-todo.png",
-		languages: ["javascript", "react", "redux"],
-		description:
-			"The simple todo app will help you to manage your day-to-day tasks. I've used browser local storage to persist data. It has some good features such as filter tasks based on status, mark all tasks as completed and removed from list, and basic CRUD.",
-	},
-	{
-		title: "Github User Search",
-		link: "https://sureshkumar1211.github.io/github-user-search",
-		type: "personal",
-		img: "/static/images/github-user-search.png",
-		languages: ["javascript", "react", "redux"],
-		description:
-			"This app built with React, Redux and other tools to search users and view their profiles on Github. I've used GitHub Api to search users on GitHub.",
-	},
-	{
-		title: "Portfolio",
-		link: "",
-		type: "personal",
-		img: "/static/images/portfolio.png",
-		languages: ["javascript", "react", "next", "typescript", "tailwind"],
-		description:
-			"A personal portfolio built with Node, React, Typescript and Tailwind. I was completed this site in a few days with responsive. Blogs section will be updated soon.",
-	},
+  {
+    title: "Youtube",
+    link: "https://sureshkumar1211.github.io/youtube-search-api",
+    type: "personal",
+    img: "https://res.cloudinary.com/dvdcrtjqj/image/upload/v1689873264/youtube_zeykkq.png",
+    languages: ["javascript", "react", "redux"],
+    description:
+      "This app will helps to fetch related videos on YouTube for the given search query. I've used GCP apikey for authentication to fetch YouTube videos.",
+  },
+  {
+    title: "Todo List",
+    link: "https://sureshkumar1211.github.io/react-redux-todo",
+    type: "personal",
+    img: "https://res.cloudinary.com/dvdcrtjqj/image/upload/v1689873262/react-todo_gwiiqk.png",
+    languages: ["javascript", "react", "redux"],
+    description:
+      "The simple todo app will help you to manage your day-to-day tasks. I've used browser local storage to persist data. It has some good features such as filter tasks based on status, mark all tasks as completed and removed from list, and basic CRUD.",
+  },
+  {
+    title: "Github User Search",
+    link: "https://res.cloudinary.com/dvdcrtjqj/image/upload/v1689873263/github-user-search_uehed7.png",
+    type: "personal",
+    img: "https://res.cloudinary.com/dvdcrtjqj/image/upload/v1689873263/github-user-search_uehed7.png",
+    languages: ["javascript", "react", "redux"],
+    description:
+      "This app built with React, Redux and other tools to search users and view their profiles on Github. I've used GitHub Api to search users on GitHub.",
+  },
+  {
+    title: "Portfolio",
+    link: "/",
+    type: "personal",
+    img: "https://res.cloudinary.com/dvdcrtjqj/image/upload/v1689873263/portfolio_c3na0p.png",
+    languages: ["javascript", "react", "next", "typescript", "tailwind"],
+    description:
+      "A personal portfolio built with Node, React, Typescript and Tailwind. I was completed this site in a few days with responsive. Blogs section will be updated soon.",
+  },
 ];
